62dcef2a1977b6e3ca74757ea28d0a50821d6892
Elizabeth Prieto <eprieto.business@gmail.com>
2017-08-28 05:02:43 -0400

    done except bonus

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+  consumer_key: "DRSwbg1sOolS8ZWzzwakHakTQ",
+  consumer_secret: "yDh9Fzn8JmN7mfbeh9fv2mPt1JnyMPTY5zFfh4RSGaIPEu71rF",
+  access_token_key: "900836391344635905-Qlj983tCrRonDjgNTpiPDSWJm0EpsVa",
+  access_token_secret: "pJCp3Ir5yE5ZaiVJEulcLUvezPsulRWnFn4P7TtaJWPwy"
+}
+
+exports.spotifyKeys = {
+  id: "902447eeda28488abf7ba59f70a65d6e",
+  secret: "f15dd34e6b424bd9abdff863327f130f"
+}
---
+++ liri.js
@@ -0,0 +1,90 @@
+var command = process.argv[2];
+var title = process.argv.slice(3).join(" ");
+var fs = require("fs");
+var request = require("request");
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var client = new twitter(keys.twitterKeys);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotifyKeys);
+
+liri();
+
+function liri(){
+	if(command === "my-tweets") {
+		tweets();
+	} else if (command === "spotify-this-song") {
+		spotifySong();
+	} else if (command === "movie-this"){
+		movie();
+	} else if (command === "do-what-it-says"){
+		random();
+	}
+}
+//	"my-tweets"
+function tweets() {
+	var params = {
+		screen_name: "88LP18",
+		count: 20
+	};
+	client.get("statuses/user_timeline", params, function(error, tweets, response) {
+		if (!error) {
+			tweets.forEach(function(tweet){
+				console.log("Date:", tweet.created_at);
+				console.log("Tweet:", tweet.text);
+			});
+		}
+	});
+}
+//	"spotify-this-song"
+function spotifySong() {
+	if (title.length < 1) {
+		title = "The Sign Ace of Base";
+	};
+	spotify.search({ type: "track", query: title }, function(err, data) {
+  	if (err) {
+    	return console.log("Error occurred:", err);
+  	} else {
+  		var song = data.tracks.items[0];
+			console.log("Song:", song.name);
+			console.log("Artist:", song.artists[0].name);
+			console.log("Album:", song.album.name);
+			console.log("Preview Link:", song.preview_url);
+		}
+	});
+}
+//	"movie-this"
+function movie() {
+	if (title.length < 1) {
+ 		title = "Mr. Nobody";
+ 	};
+	var omdb = "http://www.omdbapi.com/?t=" + title +
+ 	"&y=&plot=short&apikey=40e9cece";
+	request(omdb, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var movie = JSON.parse(body);
+			console.log("Title:",  movie.Title);
+			console.log("Plot:", movie.Plot);
+			console.log("Actors:", movie.Actors);
+			console.log("Year:", movie.Year);
+			console.log("Country:", movie.Country);
+			console.log("Language:", movie.Language);
+			console.log("IMDB Rating:", movie.imdbRating);
+			console.log("Rotten Tomatoes Rating:", movie.Ratings[1].Value);
+		} else {
+			console.log("Error:", error);
+		}
+	});
+}
+//	"do-what-it-says"
+function random() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+	if (error) {
+		return console.log(error);
+	}
+	var dataArr = data.split(",");
+	command = dataArr[0];
+	title = dataArr[1];
+	liri();
+	});
+}
